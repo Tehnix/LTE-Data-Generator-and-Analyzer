85df41719a7c535fac96f9cd9bafbb9126068ccc
Mads Kragelund <s134530@student.dtu.dk>
2015-12-02 16:12:14 +0100

    TX Controller updated
    Changes from high -> low -> high etc on each clock cycle

--- VHDL/ModelSim/TX_Controller.vhd
+++ VHDL/ModelSim/TX_Controller.vhd
@@ -7,18 +7,24 @@ port (
         rset        : in std_logic;
         tx_clk      : in std_logic
 
+
 );
 end tx_contr;
 
 architecture behavior of tx_contr is
+  signal tx_ctrl_intern : std_logic;
 
 begin
   process(tx_clk)
   begin
     if RISING_EDGE(tx_clk) then
-      tx_ctrl_sig <= '1';
+      tx_ctrl_intern <= '1';
+      if tx_ctrl_intern = '1' then
+        tx_ctrl_intern <= '0';
+      end if;
     end if;
-    
   end process;
+
+tx_ctrl_sig <= tx_ctrl_intern;
 
 end behavior;
